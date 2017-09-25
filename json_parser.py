23d38c088445d9a54f16b0c715c691eff3d468a6
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2017-09-24 20:10:28 -0700

    refactored, formatted saved json to ease readability

--- json_parser.py
+++ json_parser.py
@@ -1,15 +1,12 @@
 import json
-from pprint import pprint
 
 # open the unsorted json data
 with open('output.json') as data_file:
     data = json.load(data_file)
 # sort json by url
-sorted_results = sorted(data['repairs'], key=lambda x: x['url'])
-# reappend 'repairs' key and rebuild json
-sorted_json = {}
-sorted_json['repairs'] = sorted_results
-json_data = json.dumps(sorted_json)
-# save sorted json
+data['repairs'] = sorted(data['repairs'], key=lambda x : x['url'])
+# dump dict to json string
+json_string = json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+# save sorted json as sorted_output.json
 with open('sorted_output.json', 'w') as sorted_data_file:
-     sorted_data_file.write(json_data)
+     sorted_data_file.write(json_string)
