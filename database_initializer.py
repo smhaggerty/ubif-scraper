84f8c98df35bec5bf60a3910db3ccc3ea430d593
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2017-09-28 17:46:24 -0700

    added key for repair

--- database_initializer.py
+++ database_initializer.py
@@ -3,7 +3,7 @@ import sqlite3
 db = sqlite3.connect("prices.db")
 cursor = db.cursor()
 cursor.execute('''
-    CREATE TABLE prices(id INTEGER PRIMARY KEY, device TEXT, price TEXT, url TEXT)
+    CREATE TABLE prices(id INTEGER PRIMARY KEY, device TEXT, repair TEXT, price TEXT, url TEXT)
 ''')
 db.commit()
 db.close()
